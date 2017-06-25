ef2f04b1bb56875921363d474ca83eaaf9b1abc5
ramniwasg <ramniwas.gupta@quanteon.in>
2017-06-25 12:21:27 +0530

    final changes done

--- js/script.js
+++ js/script.js
@@ -6,18 +6,11 @@ var app = angular.module("myapp",[]);
 		vm.newItem='';
 		vm.selectedIndex='';
 		$scope.imgGallery=[];
-		vm.getItems();
-
-		function getItems() {
-			
-		}
 
 		vm.items = JSON.parse(localStorage.getItem("listData")) ==null ? [] : JSON.parse(localStorage.getItem("listData"));
-		console.log("list data on load :"+vm.items);
 		vm.filterTxt = vm.items.length>0 ? 'Filter item' : 'No Item Available';
 
 		vm.ShowBox =function () {
-			console.log("ShowBox called");
 			$("#addItemBox").show();
 			$("#addItem").hide();
 			$("#updateItem").hide();
@@ -26,28 +19,23 @@ var app = angular.module("myapp",[]);
 		}
 		
 		vm.CancelBox = function(){
-			console.log("CancelBox called");
 			$("#addItemBox").hide();
 			$("#addItem").show();	
 		}
 
 		vm.itemClicked=function(index){
 			vm.selectedIndex=index;
-			console.log("index clicked :"+vm.selectedIndex);
 		}
 		
 		vm.saveItem=function(){
-			console.log("saveItem called");
 			var item = vm.newItem;
 			vm.items.push(item);
 			localStorage.setItem("listData",JSON.stringify(vm.items));
-			console.log("Item recieved :"+item);
 			$("#addItemBox").hide();
 			$("#addItem").show();
 		}
 
 		vm.editItem =function(index){
-			console.log("edit called"+index);
 			$("#addItemBox").show();
 			$("#updateItem").show();
 			$("#saveItem").hide();
@@ -57,7 +45,6 @@ var app = angular.module("myapp",[]);
 		}
 		
 		vm.deleteItem =function(index){
-			console.log("delete called"+index);
 			vm.items.splice(index,1);
 			localStorage.setItem("listData",JSON.stringify(vm.items));
 		}
